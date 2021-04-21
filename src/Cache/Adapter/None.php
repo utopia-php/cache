e2c7f49c6e6698e86bc6c01077b4228490c5e28d
kodumbeats <brandon.leckemby@gmail.com>
2021-04-21 11:05:10 -0400

    Create none adapter

---
+++ src/Cache/Adapter/None.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Utopia\Cache\Adapter;
+
+use Utopia\Cache\Adapter;
+
+class None implements Adapter
+{
+    /**
+     * None constructor.
+     */
+    public function __construct()
+    {
+        return;
+    }
+
+    /**
+     * @param string $key
+     * @param int $ttl time in seconds
+     * @return mixed
+     */
+    public function load($key, $ttl)
+    {
+        return false;
+    }
+
+    /**
+     * @param string $key
+     * @param string $data
+     * @return bool|string
+     */
+    public function save($key, $data)
+    {
+        return false;
+    }
+
+    /**
+     * @param string $key
+     * @return bool
+     */
+    public function purge($key): bool
+    {
+        return true;
+    }
+}
