3e21e6015b156e2c06063a17493fd54d81df4661
kumar <kumar.aggarwal@aritytechnologies.com>
2022-10-29 20:46:05 +0530

    renamed file and class

--- tests/Cache/HazelcastTest.php
+++ tests/Cache/HazelcastTest.php
@@ -4,16 +4,16 @@ namespace Utopia\Tests;
 
 use Memcached as Memcached;
 use Utopia\Cache\Cache;
-use Utopia\Cache\Adapter\Hazelcast as HazelCastAdapter;
+use Utopia\Cache\Adapter\Hazelcast as HazelcastAdapter;
 use Utopia\Tests\Base;
 
-class HazelCastTest extends Base
+class HazelcastTest extends Base
 {
     public static function setUpBeforeClass(): void
     {
         $memcached = new Memcached();
         $memcached->addServer('hazelcast', 5701);
-        self::$cache = new Cache(new HazelCastAdapter($memcached));
+        self::$cache = new Cache(new HazelcastAdapter($memcached));
     }
 
     public static function tearDownAfterClass(): void
